95f212897acc3f30e7899bb820d4d3eed29d27c7
iswanto <iswanto@planbition.com>
2024-03-13 08:14:26 +0700

    fix ef context load

--- EmployeeManagement.API/Program.cs
+++ EmployeeManagement.API/Program.cs
@@ -5,10 +5,17 @@ using Microsoft.Extensions.Configuration;
 using System.Configuration;
 using EmployeeManagement.Services.Interfaces;
 using EmployeeManagement.Services.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables()
+    .Build();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,7 +23,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<EmployeeDbContext>(options =>
-       options.UseSqlServer(Configuration.GetConnectionString("EmployeeDbConnection")));
+    options.UseSqlServer(configuration.GetConnectionString("EmployeeDbConnection")));
 
 // Register the repository
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
