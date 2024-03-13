07bb57c4e41a9ca5bfca43ceb0ac5df4916b6377
iswanto <iswanto@planbition.com>
2024-03-13 08:10:55 +0700

    add service project

--- EmployeeManagement.API/Program.cs
+++ EmployeeManagement.API/Program.cs
@@ -1,3 +1,11 @@
+using EmployeeManagement.Repository.Interfaces;
+using EmployeeManagement.Repository.Repositories.EmployeeManagement.Data;
+using EmployeeManagement.Repository.Repositories;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+using EmployeeManagement.Services.Interfaces;
+using EmployeeManagement.Services.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<EmployeeDbContext>(options =>
+       options.UseSqlServer(Configuration.GetConnectionString("EmployeeDbConnection")));
+
+// Register the repository
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 
 var app = builder.Build();
 
---
+++ EmployeeManagement.Services/Services/EmployeeService.cs
@@ -0,0 +1,51 @@
+﻿using EmployeeManagement.Repository.Interfaces;
+using EmployeeManagement.Repository.Models;
+using EmployeeManagement.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Services.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeService(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository ?? throw new ArgumentNullException(nameof(employeeRepository));
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return _employeeRepository.GetAll();
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            return _employeeRepository.GetById(id);
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            _employeeRepository.Add(employee);
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            if (employee == null)
+            {
+                throw new ArgumentNullException(nameof(employee));
+            }
+
+            _employeeRepository.Update(employee);
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            _employeeRepository.Delete(id);
+        }
+    }
+}
